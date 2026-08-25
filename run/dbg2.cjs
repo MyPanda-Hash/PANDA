535f5bf1189c298823584e94de2437ae4af59dd1
x1787 <x1787@local>
2026-08-25 10:19:23 +0800

    chore: 清理临时分析脚本

--- run/dbg2.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const s = fs.readFileSync('F:/INCER/light-mes/tools/update-transfer-pureq-analysis.sql', 'utf8');
-const i = s.indexOf("'TRANSFER', '");
-const start = s.indexOf('{', i);
-const end = s.indexOf("}','", start);
-console.log('start', start, 'end', end, 'len', s.length);
-const raw = s.slice(start, end + 1);
-console.log('raw tail:', JSON.stringify(raw.slice(-120)));
-console.log('raw head:', JSON.stringify(raw.slice(0, 80)));
